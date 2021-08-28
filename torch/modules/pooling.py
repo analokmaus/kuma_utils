0e8f2d618b56d7db48648040cf1c39bacee7c889
RabotniKuma-HEOR <analokmayc@yandex.ru>
2021-08-28 19:46:52 +0900

    add AdaptiveGeM

--- torch/modules/pooling.py
+++ torch/modules/pooling.py
@@ -42,13 +42,29 @@ class AdaptiveConcatPool3d(nn.Module):
 
 class GeM(nn.Module):
     def __init__(self, p=3, eps=1e-6):
-        super(GeM, self).__init__()
+        super().__init__()
         self.p = Parameter(torch.ones(1)*p)
         self.eps = eps
 
     def forward(self, x):
-        return F.avg_pool2d(x.clamp(min=self.eps).pow(
-            self.p), (x.size(-2), x.size(-1))).pow(1./self.p)
+        return F.avg_pool2d(
+            x.clamp(min=self.eps).pow(self.p), 
+            (x.size(-2), x.size(-1))).pow(1./self.p)
 
     def __repr__(self):
-        return self.__class__.__name__ + '(' + 'p=' + '{:.4f}'.format(self.p.data.tolist()[0]) + ', ' + 'eps=' + str(self.eps) + ')'
+        return f'GeM(p={self.p}, eps={self.eps})'
+
+
+class AdaptiveGeM(nn.Module):
+    def __init__(self, size=(1, 1), p=3, eps=1e-6):
+        super().__init__()
+        self.size = size
+        self.p = Parameter(torch.ones(1)*p)
+        self.eps = eps
+
+    def forward(self, x):
+        return F.adaptive_avg_pool2d(
+            x.clamp(min=self.eps).pow(self.p), self.size).pow(1./self.p)
+
+    def __repr__(self):
+        return f'AdaptiveGeM(size={self.size}, p={self.p}, eps={self.eps})'
